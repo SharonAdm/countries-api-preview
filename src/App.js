9c1d443a0f41de080440b9309aa4ab088318cfcd
Sharon <sharonadami@Sharons-MacBook-Pro.local>
2023-05-16 13:03:02 +0330

    Cleaning up the code.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Button from "@mui/material/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-        <Button variant="contained">test</Button>
-      </header>
+    <div>
+      <h1 className="text-red-950">Hello world!</h1>
     </div>
   );
 }
