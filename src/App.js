e1584d2e1ee06245770f1a30867fdea279c74537
Sharon <sharonadami@Sharons-MacBook-Pro.local>
2023-05-16 16:01:10 +0330

    Added search bar and filter dropdown.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { FormControl, InputAdornment, MenuItem, Select, TextField } from "@mui/material";
 import NightlightOutlinedIcon from "@mui/icons-material/NightlightOutlined";
+import SearchIcon from "@mui/icons-material/Search";
 
 function App() {
+  const [selectedRegion, setSelectedRegion] = React.useState("");
+
+  const regions = ["Africa", "America", "Asia", "Europe", "Oceania"];
+
+  const handleChange = (event) => {
+    setSelectedRegion(event.target.value);
+  };
+
   return (
     <div>
       <header className="py-2 px-20 border-b-2 flex justify-between">
@@ -11,6 +21,38 @@ function App() {
           Dark Mode
         </button>
       </header>
+
+      <div className="flex justify-between">
+        <div className="my-10 mx-20">
+          <TextField
+            fullWidth
+            placeholder="search for a country..."
+            id="country-input"
+            variant="outlined"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </div>
+        <div className="my-10 mx-20">
+          <FormControl fullWidth>
+            <Select value={selectedRegion} onChange={handleChange} displayEmpty inputProps={{ "aria-label": "Without label" }}>
+              <MenuItem disabled value="">
+                <em>Filter by Region </em>
+              </MenuItem>
+              {regions.map((region) => (
+                <MenuItem key={region} value={region}>
+                  {region}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+      </div>
     </div>
   );
 }
