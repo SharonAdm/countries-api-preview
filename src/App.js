5ed7b3a31b21dd5c3ff5e0841e1e2e21a6d51c09
Sharon <sharonadami@Sharons-MacBook-Pro.local>
2023-05-16 20:47:35 +0330

    Fixated the header.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { FormControl, InputAdornment, MenuItem, Select, TextField } from "@mui/material";
-import NightlightOutlinedIcon from "@mui/icons-material/NightlightOutlined";
 import SearchIcon from "@mui/icons-material/Search";
 import axios from "axios";
 import MediaCard from "./Card";
-import { useNavigate } from "react-router-dom";
 
 function App() {
   const [selectedRegion, setSelectedRegion] = useState("");
@@ -70,17 +69,8 @@ function App() {
     console.log("ho");
     navigate("/details");
   };
-
   return (
     <div>
-      <header className="py-2 px-4 border-b-2 flex justify-between">
-        <div className="text-xl font-bold p-3">Where in the world?</div>
-        <button onClick={() => console.log("dark mode")}>
-          <NightlightOutlinedIcon />
-          Dark Mode
-        </button>
-      </header>
-
       <div className="flex justify-between">
         <div className="my-10 mx-4" onChange={handleInputChange}>
           <TextField
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NightlightOutlinedIcon from "@mui/icons-material/NightlightOutlined";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -19,6 +20,13 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <header className="py-2 px-4 border-b-2 flex justify-between">
+      <div className="text-xl font-bold p-3">Where in the world?</div>
+      <button onClick={() => console.log("dark mode")}>
+        <NightlightOutlinedIcon />
+        Dark Mode
+      </button>
+    </header>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
