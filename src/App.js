ed2aabff65c6384edd2f52ef8fd1684408a6568f
Sharon <sharonadami@Sharons-MacBook-Pro.local>
2023-05-16 13:04:50 +0330

    Added tailwind to project.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1 className="text-red-950">Hello world!</h1>
+      <h1 className="text-blue-950">Hello world!</h1>
     </div>
   );
 }
