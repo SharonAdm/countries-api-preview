9bede66acafc2888551dc8bb15722f8656d559ae
Sharon <sharonadami@Sharons-MacBook-Pro.local>
2023-05-17 17:20:40 +0330

    Added dark/light themes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,24 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      elements: {
+        dark: "#2B3945",
+        light: "#FFFFFF",
+      },
+      text: {
+        dark: "#FFFFFF",
+        light: "#111517",
+      },
+      background: {
+        dark: "#202C37",
+        light: "#FAFAFA",
+      },
+      input: {
+        light: "#858585",
+      },
+    },
   },
   plugins: [],
+  darkMode: "class",
 };
