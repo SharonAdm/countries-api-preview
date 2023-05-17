3af70855323beb1378a8e4770a9e8a5f15e13bb0
Sharon <sharonadami@Sharons-MacBook-Pro.local>
2023-05-18 02:22:35 +0330

    Fixed default mode theme.

--- src/App.js
+++ src/App.js
@@ -18,11 +18,11 @@ const theme = createTheme({
 });
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem("color-theme") === "dark");
+  const [isDarkMode, setIsDarkMode] = useState(document.documentElement.classList.toggle("dark"));
   const toggleDarkMode = () => {
-    const isDarkMode = document.documentElement.classList.toggle("dark");
-    localStorage.setItem("color-theme", isDarkMode ? "dark" : "light");
-    setIsDarkMode(isDarkMode);
+    const isDark = document.documentElement.classList.toggle("dark");
+    localStorage.setItem("color-theme", isDark ? "dark" : "light");
+    setIsDarkMode(isDark);
   };
   return (
     <ThemeProvider theme={theme}>
