b630299ec55867d589e7cbfef1b8386d61e938d4
Sharon <sharonadami@Sharons-MacBook-Pro.local>
2023-05-17 16:38:39 +0330

    Added a toggle for dark mode button.

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
-import NightlightOutlinedIcon from "@mui/icons-material/NightlightOutlined";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
 import App from "./App";
 import Details from "./Detail";
 import { ThemeProvider, createTheme } from "@mui/material";
@@ -11,32 +12,38 @@ const theme = createTheme({
       main: "#111517",
       darker: "#FFFFFF",
     },
+    light: "#FAFAFA",
+    dark: "#202C37",
   },
 });
 
 function Home() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <header className="py-2 px-4 border-b-2 flex justify-between ">
+        <header className={"py-2 px-4 border-b-2 flex justify-between " + (isDarkMode ? "dark" : "light")}>
           <div className="text-xl font-bold p-3">Where in the world?</div>
-          <button onClick={() => console.log("darkmode")}>
-            {true ? (
+          <button onClick={toggleDarkMode}>
+            {isDarkMode ? (
               <>
-                <NightlightOutlinedIcon />
+                <LightModeIcon />
                 Light Mode
               </>
             ) : (
               <>
-                <NightlightOutlinedIcon />
+                <DarkModeIcon />
                 Dark Mode
               </>
             )}
           </button>
         </header>
         <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/details" element={<Details />} />
+          <Route path="/" element={<App isDarkMode={isDarkMode} />} />
+          <Route path="/details" element={<Details isDarkMode={isDarkMode} />} />
         </Routes>
       </Router>
     </ThemeProvider>
