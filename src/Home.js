3705b43f6848f8cf4aee8c8d2cd8a86906407cf7
Sharon <sharonadami@Sharons-MacBook-Pro.local>
2023-05-17 15:52:28 +0330

    Fixed the buttons color.

--- src/Home.js
+++ src/Home.js
@@ -3,31 +3,43 @@ import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import NightlightOutlinedIcon from "@mui/icons-material/NightlightOutlined";
 import App from "./App";
 import Details from "./Detail";
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#111517",
+      darker: "#FFFFFF",
+    },
+  },
+});
 
 function Home() {
   return (
-    <Router>
-      <header className="py-2 px-4 border-b-2 flex justify-between ">
-        <div className="text-xl font-bold p-3">Where in the world?</div>
-        <button onClick={() => console.log("darkmode")}>
-          {true ? (
-            <>
-              <NightlightOutlinedIcon />
-              Light Mode
-            </>
-          ) : (
-            <>
-              <NightlightOutlinedIcon />
-              Dark Mode
-            </>
-          )}
-        </button>
-      </header>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/details" element={<Details />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <header className="py-2 px-4 border-b-2 flex justify-between ">
+          <div className="text-xl font-bold p-3">Where in the world?</div>
+          <button onClick={() => console.log("darkmode")}>
+            {true ? (
+              <>
+                <NightlightOutlinedIcon />
+                Light Mode
+              </>
+            ) : (
+              <>
+                <NightlightOutlinedIcon />
+                Dark Mode
+              </>
+            )}
+          </button>
+        </header>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/details" element={<Details />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
